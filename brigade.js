e150d68c1e4c65e55491a66b1d18a56d58006bdd
Eric Zimmer <ezimmer@barraucda.com>
2019-04-24 10:47:45 -0400

    ff

--- brigade.js
+++ brigade.js
@@ -1,10 +1,10 @@
 const { events, Job } = require("brigadier");
 events.on("simpleevent", (e, p) => {
   var job = new Job("the-demo", "alpine:3.8");
-  env_vars = JSON.parse(e.payload)
+  f = JSON.parse(e.payload)
   job.tasks = [
-    "echo Hello " + f.text,
-	"echo Im " + p.name
+    "echo Hello " + f.f,
+    "echo Im " + p.name
   ];
 
   job.run();
